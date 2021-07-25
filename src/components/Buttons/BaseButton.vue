819842be2f5eda14f53e7e22aa77dc2cb8bb554e
Lucas Fernando <lucas98fernando@gmail.com>
2021-07-25 11:42:27 -0300

    Arquivos atualizados

---
+++ src/components/Buttons/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-btn
+    :color="color"
+    v-text="textBtn"
+    @click="$emit('click', $event.target.value) && sayHi()"
+    :to="to"
+  >
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+
+  props: {
+    color: {
+      type: String,
+    },
+
+    textBtn: {
+      type: String,
+    },
+
+    to: {
+      type: String,
+    },
+  },
+
+  methods: {
+    sayHi() {
+      console.log("Oiii !!!");
+    },
+  },
+};
+</script>
---
+++ src/components/Input/BaseInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-text-field
+    :label="label"
+    :prepend-icon="prependIcon"
+    outlined
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  ></v-text-field>
+</template>
+
+<script>
+export default {
+  name: "BaseInput",
+  props: {
+    label: {
+      type: String,
+    },
+
+    prependIcon: {
+      type: String,
+    },
+
+    value: {
+      type: String,
+    },
+  },
+};
+</script>
